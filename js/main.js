f4ba1638dc1172f25c9ded47368ba92fb7348f8e
Raphael Manabat <rphlmnbt@gmail.com>
2020-10-18 23:20:23 +0800

    changed font

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const btn = document.getElementById('generate_code');
-btn.addEventListener('click', generateQR);
-
-function generateQR(){
-    const text = document.getElementById('data').value;
-    const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
-    document.getElementById('qr_img').src = url;
-    const data = new FormData();
-    data.append("json", JSON.stringify({dataName:text}));
-    fetch('http://localhost:4040/users/new', {
-        method: 'post', body: JSON.stringify({dataName:text}), headers:{"Content-Type": "application/json"}
-    }).then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data);
-    });
-}
-
 function loadMap() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -77,12 +59,11 @@ function showPosition(position) {
                 map.getSource('single-point').setData(e.result.geometry);
             });
             });
-    
-    displayPOI(position);
+    var coords = position.coords.longitude + ',' + position.coords.latitude;
+    displayPOI(coords);
 }
 
-function displayPOI(position) {
-    var coords = position.coords.longitude + ',' + position.coords.latitude;
+function displayPOI(coords) {
     console.log(coords);
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
