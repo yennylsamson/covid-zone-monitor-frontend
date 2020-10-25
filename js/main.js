1174134512f6a18a3031f8f6a56b1d63a4b58f09
Raphael Manabat <rphlmnbt@gmail.com>
2020-10-26 00:15:04 +0800

    added compare function

--- js/main.js
+++ js/main.js
@@ -1,68 +1,87 @@
 var pName;
+var cPositionLong;
+var cPositionLat;
 
 function loadMap() {
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(showPosition);
+      navigator.geolocation.watchPosition(showPosition, error, options);
     } else {
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
+
+function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+
+
+options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0
+};
   
 function showPosition(position) {
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
+    if (position.coords.longitude !== cPositionLong || position.coords.latitude !== cPositionLat) {
+        cPositionLong = position.coords.longitude;
+        cPositionLat = position.coords.latitude;
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoicnBobG1uYnQiLCJhIjoiY2tnYnl3bXM5MDBscTM0cnpwbGwwZmNneCJ9.HjYTuuq_Cx8OwDsz31ZGsg';
-            var map = new mapboxgl.Map({
-                container: 'map', // Container ID
-                style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
-                center: [position.coords.longitude, position.coords.latitude], // Starting position [lng, lat]
-                zoom: 15, // Starting zoom level
-            });
+        mapboxgl.accessToken = 'pk.eyJ1IjoicnBobG1uYnQiLCJhIjoiY2tnYnl3bXM5MDBscTM0cnpwbGwwZmNneCJ9.HjYTuuq_Cx8OwDsz31ZGsg';
+                var map = new mapboxgl.Map({
+                    container: 'map', // Container ID
+                    style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
+                    center: [position.coords.longitude, position.coords.latitude], // Starting position [lng, lat]
+                    zoom: 15, // Starting zoom level
+                });
 
-            var marker = new mapboxgl.Marker() // initialize a new marker
-                .setLngLat([position.coords.longitude, position.coords.latitude]) // Marker [lng, lat] coordinates
-                .addTo(map); // Add the marker to the map
-            
-            var geocoder = new MapboxGeocoder({ // Initialize the geocoder
-                accessToken: mapboxgl.accessToken, // Set the access token
-                mapboxgl: mapboxgl, // Set the mapbox-gl instance
-                marker: false, // Do not use the default marker style
-                // bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
-                proximity: {
-                    longitude: -122.25948,
-                    latitude: 37.87221
-                } // Coordinates of UC Berkeley
-            });
+                var marker = new mapboxgl.Marker() // initialize a new marker
+                    .setLngLat([position.coords.longitude, position.coords.latitude]) // Marker [lng, lat] coordinates
+                    .addTo(map); // Add the marker to the map
+                
+                var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+                    accessToken: mapboxgl.accessToken, // Set the access token
+                    mapboxgl: mapboxgl, // Set the mapbox-gl instance
+                    marker: false, // Do not use the default marker style
+                    // bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
+                    proximity: {
+                        longitude: -122.25948,
+                        latitude: 37.87221
+                    } // Coordinates of UC Berkeley
+                });
 
-            map.on('load', function() {
-            map.addSource('single-point', {
-                type: 'geojson',
-                data: {
-                type: 'FeatureCollection',
-                features: []
-                }
-            });
+                map.on('load', function() {
+                map.addSource('single-point', {
+                    type: 'geojson',
+                    data: {
+                    type: 'FeatureCollection',
+                    features: []
+                    }
+                });
 
-            map.addLayer({
-                id: 'point',
-                source: 'single-point',
-                type: 'circle',
-                paint: {
-                'circle-radius': 10,
-                'circle-color': '#448ee4'
-                }
-            });
+                map.addLayer({
+                    id: 'point',
+                    source: 'single-point',
+                    type: 'circle',
+                    paint: {
+                    'circle-radius': 10,
+                    'circle-color': '#448ee4'
+                    }
+                });
 
-            // Listen for the `result` event from the Geocoder
-            // `result` event is triggered when a user makes a selection
-            //  Add a marker at the result's coordinates
-            geocoder.on('result', function(e) {
-                map.getSource('single-point').setData(e.result.geometry);
-            });
-            });
-    var coords = position.coords.longitude + ',' + position.coords.latitude;
-    displayPOI(coords);
+                // Listen for the `result` event from the Geocoder
+                // `result` event is triggered when a user makes a selection
+                //  Add a marker at the result's coordinates
+                geocoder.on('result', function(e) {
+                    map.getSource('single-point').setData(e.result.geometry);
+                });
+                });
+        var coords = position.coords.longitude + ',' + position.coords.latitude;
+        displayPOI(coords);
+    }
+    
 }
 
 function displayPOI(coords) {
@@ -75,14 +94,15 @@ function displayPOI(coords) {
         if (name !== pName) {
             document.getElementById("placename").innerText = name;
             console.log(myObj.features[0].place_name);
-            listPlace(dateTime, name);
+            listPlace(date, time, name);
             pName = name;
         }
     }
     };
     xmlhttp.open("GET", "https://api.mapbox.com/geocoding/v5/mapbox.places/"+coords+".json?types=poi.landmark&limit=1&access_token=pk.eyJ1IjoicnBobG1uYnQiLCJhIjoiY2tnYnl3bXM5MDBscTM0cnpwbGwwZmNneCJ9.HjYTuuq_Cx8OwDsz31ZGsg", true);
     xmlhttp.send();
-    var dateTime = getDate();
+    var date = getDate();
+    var time = getTime();
 }
 
 function saveData(){
@@ -102,7 +122,7 @@ function saveData(){
         dataAddress: user_address,
         dataPassword: user_password
     }));
-    fetch('http://localhost:4040/users/register', {
+    fetch('http://localhost:4040/users/new', {
         method: 'post', body: JSON.stringify({
             dataLastName: last_name,
             dataFirstName: first_name,
@@ -116,7 +136,8 @@ function saveData(){
         return response.json();
     }).then(function(dataList) {
         console.log(dataList);
-        getUsers();
+        window.location.href ="http://localhost:8080/covid-zone-monitor-frontend/login.html"
+
     });
 
     
@@ -137,22 +158,17 @@ function getUsers() {
 
 // first_name, last_name, date_Time, place_Name
 
-function listPlace(date_Time, place_Name) { 
+function listPlace(date, time, place_Name) { 
     last_name = sessionStorage.getItem('userLastName');
     first_name = sessionStorage.getItem('userFirstName');
-    const dataList = new FormData();
-    dataList.append("json", JSON.stringify({
-        dataLastName: last_name,
-        dataFirstName: first_name,
-        dateTime: "date_Time",
-        placeName: "place_Name",
-    }));
+
 
     fetch('http://localhost:4040/users/log', {
         method: 'post', body: JSON.stringify({
             // dataLastName: last_name,
             // dataFirstName: first_name,
-            dateTime: date_Time,
+            dataTime: time,
+            dataDate: date,
             placeName: place_Name,
             dataLastName: last_name,
             dataFirstName: first_name,
@@ -171,10 +187,17 @@ function listPlace(date_Time, place_Name) {
 function getDate() {
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var d = new Date();
-    var min = (d.getMinutes()<10?("0"+d.getMinutes()):(d.getMinutes()));
-    var dateTime = "Date: " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear() + " " + "Time: " + d.getHours() + ":" + min;
+    var date = "Date: " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
 
-    return dateTime;
+    return date;
+}
+
+function getTime() {
+    var d = new Date();
+    var min = (d.getMinutes()<10?("0"+d.getMinutes()):(d.getMinutes()));
+    var time = "Time: " + d.getHours() + ":" + min;
+
+    return time;
 }
 
 
@@ -194,9 +217,9 @@ function checkCreds() {
             // User last name stored in session storage
             sessionStorage.setItem('userLastName', dataList.data[0].dataLastName);
             sessionStorage.setItem('userFirstName', dataList.data[0].dataFirstName);
-            // window.location.href ="http://localhost:8080/covid-zone-monitor-frontend/notification.html"
-            window.location.href ="http://localhost/covid-zone-monitor-frontend/notification.html"
-            console.log(localStorage.setItem('userFirstName', dataList.data[0].dataFirstName));
+            window.location.href ="http://localhost:8080/covid-zone-monitor-frontend/notification.html"
+            // window.location.href ="http://localhost/covid-zone-monitor-frontend/notification.html"
+            
             
         } else{
             window.alert("Wrong Email or Password");
@@ -204,10 +227,10 @@ function checkCreds() {
     });
 }
 
-
-function getPostiveLocation() {
-    // lastName = localStorage.getItem('userLastName');
-    lastName = "last_name";
+//add users location to positive table
+function addPostiveLocation() {
+    lastName = sessionStorage.getItem('userLastName');
+    console.log(lastName);
     fetch('http://localhost:4040/users/loc', {
         method: 'post', body: JSON.stringify({
             dataLastName : lastName,
@@ -217,14 +240,14 @@ function getPostiveLocation() {
       return response.json();
     }).then(function(dataList) {
         dataList.data.forEach(function (e) {
-            // console.log("Placename: "+e.placeName);
-            // console.log("date: "+e.dateTime);
+            console.log("Placename: "+e.placeName);
+            console.log("Date: "+e.dataDate);
             // add to the database
             fetch('http://localhost:4040/users/pos', {
             method: 'post', body: JSON.stringify({
 
-                dateTime: e.dateTime,
-                location: e.placeName,
+                dataDate: e.dataDate,
+                placeName: e.placeName,
             }), 
                 headers:{"Content-Type": "application/json"}
             }).then(function(response) {
@@ -237,9 +260,9 @@ function getPostiveLocation() {
 }
 
 
-function showPostiveLocation() {
-    // lastName = localStorage.getItem('userLastName');
-    lastName = "last_name";
+function showLocationHistory() {
+    lastName = sessionStorage.getItem('userLastName');
+    // lastName = "last_name";
     fetch('http://localhost:4040/users/loc', {
         method: 'post', body: JSON.stringify({
             dataLastName : lastName,
@@ -250,18 +273,58 @@ function showPostiveLocation() {
     }).then(function(dataList) {
         dataList.data.forEach(function (e) {
              console.log("Placename: "+e.placeName);
-             console.log("date: "+e.dateTime);
+             console.log("Date: "+e.dataDate);
              var table = document.getElementById("historyTable");
              var row = table.insertRow(-1);
              var cell1 = row.insertCell(0);
              var cell2 = row.insertCell(1);
-             cell1.innerHTML = e.dateTime;
+             cell1.innerHTML = e.dataDate;
              cell2.innerHTML = e.placeName;
 
         });
     });
-
 }
+
+//get locations with covid positive
+function getPositiveLocation() {
+    fetch('http://localhost:4040/users/getpos', {
+        method: 'get'
+    }
+    
+    ).then(function(response) {
+      return response.json();
+    }).then(function(positiveLocations) {
+        var comparedLocations = {
+            "locations":[
+          ]
+        }
+
+        lastName = sessionStorage.getItem('userLastName');
+        fetch('http://localhost:4040/users/loc', {
+            method: 'post', body: JSON.stringify({
+                dataLastName : lastName,
+            }),
+                headers:{"Content-Type": "application/json"}
+        }).then(function(response) {
+        return response.json();
+        }).then(function(dataList) {
+            dataList.data.forEach(function (e) {
+                 positiveLocations.data.forEach(function (f) {
+                    if (e.placeName === f.placeName && e.dataDate === f.dataDate) {
+                        var newPlaceName = f.placeName;
+                        var newDataDate = f.dataDate;
+                        comparedLocations['locations'].push({"placeName":newPlaceName,"dataDate":newDataDate});
+                        console.log(comparedLocations);
+                    
+                    }
+                 })
+            });
+        });
+    });
+}
+
+
+
 
 //reset localstorage
 function temporaryReset() {
